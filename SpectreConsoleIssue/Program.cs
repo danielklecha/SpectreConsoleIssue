c900df0a35f69c1eaca63929973b56f79e3bdb91
Daniel Klecha <daniel.klecha@gmail.com>
2022-02-01 19:04:35 +0100

    Add project files.

---
+++ SpectreConsoleIssue/Program.cs
@@ -0,0 +1,14 @@
+﻿using Spectre.Console;
+var uri = await new TextPrompt<Uri?>( $"url" )
+    .AllowEmpty()
+    .Validate( value =>
+    {
+        if ( !( value?.IsAbsoluteUri ?? true ) )
+            return ValidationResult.Error( "[red]It must be absolute url[/]" );
+        return ValidationResult.Success();
+    } )
+    .ShowAsync( AnsiConsole.Console, CancellationToken.None );
+if ( uri == null )
+    AnsiConsole.WriteLine( "You selected empty value" );
+else
+    AnsiConsole.WriteLine( $"You selected {uri.AbsoluteUri}" );
